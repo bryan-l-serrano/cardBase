ab527c79ca68515c1ae8e2ae7a3c3781a16bd81a
bryan-l-serrano <bryan.l.serrano@gmail.com>
2020-05-17 21:48:44 -0400

    changed main to show how to blit an array of images

--- main.py
+++ main.py
@@ -2,17 +2,22 @@ from card import *
 from basicDeck import *
 import pygame
 
+width = 800
+height = 600
+screen = pygame.display.set_mode([width, height])
+
+def display(ob, loc):
+    for x in range(0,len(ob)):
+        screen.blit(ob[x], loc[x])
+
 def main():
 
     pygame.init()
+    numberOfCardsDrawn = 0
     x = 0
     y = 0
-    width = 800
-    height = 600
-    screen = pygame.display.set_mode([width, height])
 
     running = True
-    lastCard = 0
     player1 = basic()
     player2 = basic()
     gameDeck = basic()
@@ -20,8 +25,9 @@ def main():
     gameDeck.shuffle()
     cardBack = pygame.transform.rotozoom(pygame.image.load('./PNG/red_back.png').convert(), 0, 0.2)
     screen.fill((0,170,0))
-    screen.blit(cardBack, (width * .3, height * .6))
     pygame.display.flip()
+    displayObjects =[cardBack]
+    locations = [(width * .02, height * .6)]
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -29,20 +35,21 @@ def main():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 x,y = event.pos
                 if gameDeck.deck:
-                     screen.blit(cardBack, (width * .3, height * .6))
-                     if cardBack.get_rect(topleft=(width*.3, height* .6)).collidepoint(x,y):
+                     if cardBack.get_rect(topleft=(width* 0.02, height* .6)).collidepoint(x,y):
                         cardDrawn = gameDeck.drawCard()
+                        numberOfCardsDrawn += 1
                         player1.addCard(cardDrawn)
-                        cardDis = pygame.transform.rotozoom(pygame.image.load(cardDrawn.image).convert(), 0, 0.2)
-                        lastCard = cardDis
-                        screen.blit(cardDis, (width * .6, height * .6))
+                        thisCard = pygame.transform.rotozoom(pygame.image.load(cardDrawn.image).convert(), 0, 0.2)
+                        displayObjects.append(thisCard)
+                        locations.append((width * 0.2 +(numberOfCardsDrawn * 6), height * 0.6))
                         print(cardDrawn.id)
-                if not gameDeck.deck:
-                    screen.fill((0,170,0))
-                    screen.blit(lastCard, (width * .6, height * .6))
-
-        pygame.display.flip()
-
+                        print(len(gameDeck.deck))
+                        if len(gameDeck.deck) == 0:
+                            del displayObjects[0]
+                            del locations[0]
+        screen.fill((0,170,0))
+        display(displayObjects, locations)
+        pygame.display.update()
     pygame.quit()
 if __name__ == "__main__":
     main()
