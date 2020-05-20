be3fc6534807be692fdf09b9f1ea3ab476f0bd6a
bryan-l-serrano <bryan.l.serrano@gmail.com>
2020-05-19 22:52:39 -0400

    edited card class to default to integer memory is now a functional game

--- card.py
+++ card.py
@@ -1,5 +1,5 @@
 class card:
-    def __init__(self, id = 'gray_back', value = ""):
+    def __init__(self, id = 'gray_back', value = 0):
         self.id = id
         self.value = value
         self.image = './PNG/' + id + '.png'
--- memory.py
+++ memory.py
@@ -1,20 +1,26 @@
 from card import *
 from basicDeck import *
 import pygame
+import time
 
-#initialize pygame
+# initialize pygame
 pygame.init()
 
-#creates screen and width/height variables
+# creates screen and width/height variables
 width = 800
 height = 600
 screen = pygame.display.set_mode([width, height])
 
 
-#function to blit all images
+# function to blit all images
 def display(ob, loc):
-    for x in range(0,len(ob)):
+    for x in range(0, len(ob)):
         screen.blit(ob[x], loc[x])
+
+def refresh(displayObjects, locations):
+    screen.fill((0, 170, 0))
+    display(displayObjects, locations)
+    pygame.display.update()
 
 
 def main():
@@ -25,47 +31,65 @@ def main():
 
     cardBack = pygame.transform.rotozoom(pygame.image.load('./PNG/red_back.png').convert(), 0, 0.08)
 
-
-    #create lists to store image data and locations
+    # create lists to store image data and locations
     displayObjects = []
     locations = []
-
-    #create locations for all cards
+    cardsSelected = []
+    # create locations for all cards
     for i in range(0, 13):
         for j in range(0, 4):
-            locations.append((5+(i * 60), 10+(j * 150)))
+            locations.append((5 + (i * 60), 10 + (j * 150)))
 
-    #create gameDeck from deck and card classes and shuffle it
+    # create gameDeck from deck and card classes and shuffle it
     gameDeck = basic()
     gameDeck.fullDeck()
     gameDeck.shuffle()
 
-    #add cardBacks into displayObject array
+    # add cardBacks into displayObject array
     for i in range(0, len(gameDeck.deck)):
         displayObjects.append(cardBack)
 
-    #game loop
+    # game loop
     while running:
         for event in pygame.event.get():
+            #checks if exit
             if event.type == pygame.QUIT:
                 running = False
+            #checks is mouse clicked
             if event.type == pygame.MOUSEBUTTONDOWN:
-                x,y = event.pos
-                print(str(x) + ":" + str(y))
+                #gets point where mouse was clicked
+                x, y = event.pos
 
-                #
-                #           To do
-                # find collision conditions for all values
-                # logic for displaying selected cards
-                # logic for removing matching cards
-                #
-                #
-        #refresh screen
-        screen.fill((0, 170, 0))
-        display(displayObjects, locations)
-        pygame.display.update()
+                for i in range(0, len(locations)):
+                    #checks to see if clicked on a card
+                    if displayObjects[i].get_rect(topleft=(locations[i][0], locations[i][1])).collidepoint(x, y) and not i in cardsSelected:
+                        cardsSelected.append(i)
+                        print("collision with item: " + str(i))
+                        todis = pygame.transform.rotozoom(pygame.image.load(gameDeck.deck[i].image).convert(), 0, 0.08)
+                        displayObjects[i] = todis
+                        refresh(displayObjects, locations)
+                #if 2 cards have been clicked checks to see if equal value
+                if len(cardsSelected) == 2:
+                    cardsSelected.sort(reverse=True)
+                    time.sleep(0.75)
+                    if gameDeck.deck[cardsSelected[0]].value == gameDeck.deck[cardsSelected[1]].value:
+                        #if equal remove both cards from display
+                        locations.pop(cardsSelected[0])
+                        locations.pop(cardsSelected[1])
+                        displayObjects.pop(cardsSelected[0])
+                        displayObjects.pop(cardsSelected[1])
+                        cardsSelected = []
+                    else:
+                        # if not equal display backs of cards again
+                        displayObjects[cardsSelected[0]] = cardBack
+                        displayObjects[cardsSelected[1]] = cardBack
+                        cardsSelected = []
+
+        # refresh screen
+        refresh(displayObjects, locations)
 
 
     pygame.quit()
+
 
 main()
